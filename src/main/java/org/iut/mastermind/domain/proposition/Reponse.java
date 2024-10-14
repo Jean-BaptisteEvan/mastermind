f571bf20be50fcd47d38ad1943997057ebf6858e
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2024-10-14 17:18:38 +0200

    suppresion de l'attribut position de reponse

--- src/main/java/org/iut/mastermind/domain/proposition/Reponse.java
+++ src/main/java/org/iut/mastermind/domain/proposition/Reponse.java
@@ -7,7 +7,7 @@ import static java.util.Collections.unmodifiableList;
 public class Reponse {
     private final String motSecret;
     private final List<Lettre> resultat = new ArrayList<>();
-    private int position;
+    //private int position;
 
     public Reponse(String mot) {
         this.motSecret = mot;
@@ -22,12 +22,9 @@ public class Reponse {
     // du mot proposé
     public void compare(String essai) {
         char[] res = essai.toCharArray();
-        for(char c : res){
-            Lettre l = evaluationCaractere(c);
-            this.resultat.add(l);
-            this.position++;
+        for(int i = 0;i < res.length;i++){
+            this.resultat.add(evaluationCaractere(res[i],i));
         }
-        this.position = 0;
     }
 
     // si toutes les lettres sont placées
@@ -45,9 +42,9 @@ public class Reponse {
     }
 
     // renvoie le statut du caractère
-    private Lettre evaluationCaractere(char carCourant) {
+    private Lettre evaluationCaractere(char carCourant,int pos) {
         if(estPresent(carCourant)){
-            if(estPlace(carCourant)){
+            if(estPlace(carCourant,pos)){
                 return Lettre.PLACEE;
             }
             return Lettre.NON_PLACEE;
@@ -67,7 +64,7 @@ public class Reponse {
     }
 
     // le caractère est placé dans le mot secret
-    private boolean estPlace(char carCourant) {
-        return motSecret.charAt(position) == carCourant;
+    private boolean estPlace(char carCourant,int pos) {
+        return motSecret.charAt(pos) == carCourant;
     }
 }
